ca2a50a9dbacd90f9865c57344daa8d44304b757
xiaorong1176 <750106615@qq.com>
2015-05-18 22:57:12 +0800

    MainActivity

--- src/com/echo/robat/ChatMessageAdapter.java
+++ src/com/echo/robat/ChatMessageAdapter.java
@@ -1,30 +1,103 @@
 package com.echo.robat;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import com.echo.bean.ChatMessage;
+import com.echo.bean.ChatMessage.Type;
+import com.echo.robatbyecho.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 
 public class ChatMessageAdapter extends BaseAdapter {
+	
+	private LayoutInflater mInflater;
+	private List<ChatMessage> mDatas;
+	
+	public ChatMessageAdapter(Context context, List<ChatMessage> mDatas)
+	{
+		mInflater = LayoutInflater.from(context);
+		this.mDatas = mDatas;
+	}
 
 	public int getCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return mDatas.size();
 	}
 
-	public Object getItem(int arg0) {
+	public Object getItem(int position) {
 		// TODO Auto-generated method stub
-		return null;
+		return mDatas.get(position);
 	}
 
-	public long getItemId(int arg0) {
+	public long getItemId(int position) {
 		// TODO Auto-generated method stub
-		return 0;
+		return position;
+	}
+	
+	public int getItemViewType(int position){
+		ChatMessage chatMessage = mDatas.get(position);
+		if (chatMessage.getType() == Type.INCOMING)
+		{
+			return 0;
+		}
+		return 1;
+	}
+	
+	@Override
+	public int getViewTypeCount()
+	{
+		return 2;
 	}
 
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
+	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		return null;
+		ChatMessage chatMessage = mDatas.get(position);
+		ViewHolder viewHolder = null;
+		if (convertView == null)
+		{
+			// 通过ItemType设置不同的布局
+			if (getItemViewType(position) == 0)
+			{
+				convertView = mInflater.inflate(R.layout.item_from_msg, parent,
+						false);
+				viewHolder = new ViewHolder();
+				viewHolder.mDate = (TextView) convertView
+						.findViewById(R.id.id_form_msg_date);
+				viewHolder.mMsg = (TextView) convertView
+						.findViewById(R.id.id_from_msg_info);
+			} else
+			{
+				convertView = mInflater.inflate(R.layout.item_to_msg, parent,
+						false);
+				viewHolder = new ViewHolder();
+				viewHolder.mDate = (TextView) convertView
+						.findViewById(R.id.id_to_msg_date);
+				viewHolder.mMsg = (TextView) convertView
+						.findViewById(R.id.id_to_msg_info);
+			}
+			convertView.setTag(viewHolder);
+		} else
+		{
+			viewHolder = (ViewHolder) convertView.getTag();
+		}
+		// 设置数据
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		viewHolder.mDate.setText(df.format(chatMessage.getDate()));
+		viewHolder.mMsg.setText(chatMessage.getMsg());
+		return convertView;
+	}
+	
+	private final class ViewHolder
+	{
+		TextView mDate;
+		TextView mMsg;
 	}
 
 }
--- src/com/echo/robat/MainActivity.java
+++ src/com/echo/robat/MainActivity.java
@@ -1,16 +1,26 @@
 package com.echo.robat;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import com.echo.bean.ChatMessage;
+import com.echo.bean.ChatMessage.Type;
 import com.echo.robatbyecho.R;
+import com.echo.utils.HttpUtils;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
@@ -22,6 +32,20 @@ public class MainActivity extends Activity {
 	private EditText mInputMsg;
 	private Button mSendMsg;
 	
+
+	private Handler mHandler = new Handler()
+	{
+		public void handleMessage(android.os.Message msg)
+		{
+			// 等待接收，子线程完成数据的返回
+			ChatMessage fromMessge = (ChatMessage) msg.obj;
+			mDatas.add(fromMessge);
+			mAdapter.notifyDataSetChanged();
+			mMsgs.setSelection(mDatas.size()-1);
+		};
+
+	};
+	
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
@@ -30,11 +54,54 @@ public class MainActivity extends Activity {
 		
 		initView();
 		initDatas();
+		initListener();
+	}
+
+	private void initListener() {
+		// TODO Auto-generated method stub
+		mSendMsg.setOnClickListener(new OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				final String toMsg = mInputMsg.getText().toString();
+				if (TextUtils.isEmpty(toMsg))
+				{
+					Toast.makeText(MainActivity.this, "发送消息不能为空！",
+							Toast.LENGTH_SHORT).show();
+					return;
+				}
+				
+				ChatMessage toMessage = new ChatMessage();
+				toMessage.setDate(new Date());
+				toMessage.setMsg(toMsg);
+				toMessage.setType(Type.OUTCOMING);
+				mDatas.add(toMessage);
+				mAdapter.notifyDataSetChanged();
+				mMsgs.setSelection(mDatas.size()-1);
+				
+				mInputMsg.setText("");
+				
+				new Thread()
+				{
+					public void run()
+					{
+						ChatMessage fromMessage = HttpUtils.sendMessage(toMsg);
+						Message m = Message.obtain();
+						m.obj = fromMessage;
+						mHandler.sendMessage(m);
+					};
+				}.start();
+
+			}
+		});
 	}
 
 	private void initDatas() {
 		// TODO Auto-generated method stub
-		
+		mDatas = new ArrayList<ChatMessage>();
+		mDatas.add(new ChatMessage("你好，小慕为您服务", Type.INCOMING, new Date()));
+		mAdapter = new ChatMessageAdapter(this, mDatas);
+		mMsgs.setAdapter(mAdapter);
 	}
 
 	private void initView() {
