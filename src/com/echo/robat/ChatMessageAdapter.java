9271090d6a8f529525ac52d0ec2191d68603b9dc
xiaorong1176 <750106615@qq.com>
2015-05-18 22:40:36 +0800

    ChatMessageAdapter

---
+++ src/com/echo/robat/ChatMessageAdapter.java
@@ -0,0 +1,30 @@
+package com.echo.robat;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+
+public class ChatMessageAdapter extends BaseAdapter {
+
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public View getView(int arg0, View arg1, ViewGroup arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/com/echo/robat/MainActivity.java
+++ src/com/echo/robat/MainActivity.java
@@ -1,7 +1,47 @@
 package com.echo.robat;
 
+import java.util.List;
+
+import com.echo.bean.ChatMessage;
+import com.echo.robatbyecho.R;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	
+	//一般ListView需要绑定一个适配器，适配器需要绑定一个数据源
+	private ListView mMsgs;
+	private ChatMessageAdapter mAdapter;
+	private List<ChatMessage> mDatas;
+	
+	private EditText mInputMsg;
+	private Button mSendMsg;
+	
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		
+		initView();
+		initDatas();
+	}
+
+	private void initDatas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initView() {
+		// TODO Auto-generated method stub
+		mMsgs = (ListView) findViewById(R.id.id_listview_msgs);
+		mInputMsg = (EditText) findViewById(R.id.id_input_msg);
+		mSendMsg = (Button) findViewById(R.id.id_send_msg);
+	}
 
 }
